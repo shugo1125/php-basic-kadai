919637ce389278d9ee17a32669056c8a9d853591
shugo1125 <iamsyu25@icloud.com>
2024-09-16 17:05:54 +0900

    kadai_web_appを追加

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,51 @@
+<?php
+// POSTリクエストから入力データを取得
+$name = $_POST['user_name'];
+$age = $_POST['user_age'];
+$category = $_POST['user_category'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>確認画面</title>
+</head>
+
+<body>
+  <h2>入力内容をご確認ください。</h2>
+  <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+
+  <table border="1">
+    <tr>
+      <th>項目</th>
+      <th>入力内容</th>
+      </th>
+    <tr>
+      <td>社員名</td>
+      <td>
+        <?php echo $name; ?>
+      </td>
+    </tr>
+    <tr>
+      <td>年齢</td>
+      <td>
+        <?php echo $name; ?>
+      </td>
+    </tr>
+    <tr>
+      <td>所属部署</td>
+      <td>
+        <?php echo $category; ?>
+      </td>
+    </tr>
+  </table>
+
+  <p>
+    <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+    <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+  </p>
+</body>
+
+</html>
