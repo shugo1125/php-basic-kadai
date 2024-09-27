4ba7b2ec9a7cdfac067aeb559474554f0fa8fd5a
shugo1125 <iamsyu25@icloud.com>
2024-09-27 15:30:04 +0900

    2回目のコミット

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -1,7 +1,7 @@
 <?php
-$personal_data = ['id' => '1', '名前' => '侍太郎', '年齢' => '30'];
+$personal_data = ['名前' => '玉ねぎ', '値段' => '200', '産地' => '北海道'];
 
 // 連想配列$personal_dataのキーと値を1つずつ順番に出力する
 foreach ($personal_data as $key => $value) {
-  echo "{$key}:{$value}<br>";
+  echo "{$key}：{$value}<br>";
 }
